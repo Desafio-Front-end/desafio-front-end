760160c0698919213813a8aed96810b4d1a8d21e
Bruna Adam <brunaadam54@gmail.com>
2025-03-10 19:59:06 -0300

    Alteração dos links deimagem no home-aluno

--- src/pages/home-aluno/home-aluno.jsx
+++ src/pages/home-aluno/home-aluno.jsx
@@ -1,45 +1,46 @@
 import { Button, Card, CardContent, MenuItem, TextField } from '@mui/material';
-import'./home-aluno.css';
-import ImgDisciplina from 'C:/Users/giuli/desafio-front-end/src/assets/img-disciplina.png';
-import ImgMatricula from 'C:/Users/giuli/desafio-front-end/src/assets/img-matricula.png';
-import ImgTurma from 'C:/Users/giuli/desafio-front-end/src/assets/img-turma.png';
+import './home-aluno.css';
+import ImgDisciplina from './../../assets/img-disciplina.png';
+import ImgMatricula from './../../assets/img-matricula.png';
+import ImgTurma from './../../assets/img-turma.png';
 
 
 export const HomeAluno = () => {
     return (
         <>
-        <div>
-            <h2 className='titulo-paginainicial'>
-                BEM-VINDO, nome do aluno
-            </h2>
-        </div>
+            <div>
+                <h2 className='titulo-paginainicial'>
+                    BEM-VINDO, nome do aluno
+                </h2>
+            </div>
 
-        <div className='espacamento-titulo-cards'></div>
-        
-        <div className='container-home-instituicao'>    
-        <div>
-            <Card sx={{maxWidth: 175, maxHeight:300 ,padding: '10px' }}>
-                <CardContent className='card-cadastrarturma'>
-                    <img src={ImgMatricula} />
-                    <h2 className='cadastrar-turma-titulo'>
-                        FAZER MATRÍCULA
-                    </h2>
-                </CardContent>
-            </Card>
-        </div>
-        <div className='espacamento-cards'>
+            <div className='espacamento-titulo-cards'></div>
 
-        </div>
-        <div>
-            <Card sx={{maxWidth: 175, maxHeight:300 ,padding: '10px' }}>
-                <CardContent className='card-cadastrardisciplina'>
-                    <img src={ImgDisciplina} />
-                    <h2 className='cadastrar-disciplina-titulo'>
-                        DISCIPLINAS
-                    </h2>
-                </CardContent>
-            </Card>
-        </div>
-        </div>
+            <div className='container-home-instituicao'>
+                <div>
+                    <Card sx={{ maxWidth: 175, maxHeight: 300, padding: '10px' }}>
+                        <CardContent className='card-cadastrarturma'>
+                            <img src={ImgMatricula} />
+                            <h2 className='cadastrar-turma-titulo'>
+                                FAZER MATRÍCULA
+                            </h2>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className='espacamento-cards'>
 
-        </>)}
+                </div>
+                <div>
+                    <Card sx={{ maxWidth: 175, maxHeight: 300, padding: '10px' }}>
+                        <CardContent className='card-cadastrardisciplina'>
+                            <img src={ImgDisciplina} />
+                            <h2 className='cadastrar-disciplina-titulo'>
+                                DISCIPLINAS
+                            </h2>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+
+        </>)
+}
