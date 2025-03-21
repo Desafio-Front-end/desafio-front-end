427b51aec7ea6ac4def18d48f5d90bfd9afe8b60
Bruna Adam <brunaadam54@gmail.com>
2025-03-20 21:21:19 -0300

    Refinamento página inicial

--- src/pages/pagina-inicial/pagina-inicial.jsx
+++ src/pages/pagina-inicial/pagina-inicial.jsx
@@ -18,14 +18,14 @@ const opcoesTipoUsuarios = [
 export const PaginaInicial = () => {
     return (
         <>
-            <div>
+            
+
+            <div className='container-cadastrese'>
                 <h2 className='titulo-paginainicial'>
                     BEM-VINDO
                 </h2>
-            </div>
-
-            <div className='container-cadastrese'>
-                <Card sx={{ maxWidth: 500, padding: '24px' }} >
+                <div className='container-card'>
+                <Card sx={{ width: 275, padding: '24px' }} >
                     <CardContent className='card-cadastrese'>
                         <h2 className='card-cadastro-titulo'>
                             CADASTRE-SE
@@ -33,21 +33,24 @@ export const PaginaInicial = () => {
                         <TextField
                             className='card-cadastrese-input'
                             label="Nome"
+                            size='small'
                         />
                         <TextField
                             className='card-cadastrese-input'
                             label="Email"
+                            size='small'
                         />
                         <TextField
                             className='card-cadastrese-input'
                             label="Senha"
+                            size='small'
                         />
                         <TextField
                             className='card-cadastrese-input-selector'
                             label="Tipo de usuário"
+                            size='small'
                             select
-                            defaultValue="1"
-                            helperText="Selecione o tipo de usuário">
+                            defaultValue="1">
                             {opcoesTipoUsuarios.map((option) => (
                                 <MenuItem key={option.value} value={option.value}>
                                     {option.label}
@@ -58,10 +61,8 @@ export const PaginaInicial = () => {
                         <Button variant="contained" className='card-cadastrese-button'>CADASTRAR</Button>
                     </CardContent>
                 </Card>
-                <div className='espacamento-cards'>
 
-                </div>
-                <Card sx={{ maxWidth: 500, padding: '24px', display: 'flex', alignItems: 'center' }} >
+                <Card sx={{ width: 275, padding: '24px', display: 'flex', alignItems: 'center' }} >
                     <CardContent className='card-login'>
                         <h2 className='card-cadastro-titulo'>
                             LOGIN
@@ -69,15 +70,19 @@ export const PaginaInicial = () => {
                         <TextField
                             className='card-login-input'
                             label="Email"
+                            size='small'
                         />
                         <TextField
                             className='card-login-input'
                             label="Senha"
+                            size='small'
                         />
                         <Button variant="contained" className='card-login-button'>ENTRAR</Button>
 
                     </CardContent>
                 </Card>
+                </div>
+                
             </div >
 
         </>
